5e80d31271a8ac4203ddd5e64441a9ff91430f8b
vearse <versatilekaywize94@gmail.com>
2022-06-15 18:48:22 +0100

    Jour Task -1

--- scripts/background.js
+++ scripts/background.js
@@ -3,11 +3,12 @@ console.log("Background Extention Runing")
 chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
     console.log(tabs[0].url);
 }); 
-
+ 
 chrome.runtime.onInstalled.addListener((reason) => {
     if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
-        chrome.tabs.create({
-        url: 'onboarding.html'
-        });
+        console.log('Installing');
+        // chrome.tabs.create({
+        //     url: 'onboarding.html'
+        // });
     }
 });
