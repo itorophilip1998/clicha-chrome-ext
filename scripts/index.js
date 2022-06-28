830fc3be62558639ba6e003a4f9a344fae50964f
vearse <versatilekaywize94@gmail.com>
2022-06-28 16:14:36 +0100

    Google Search Completion

--- scripts/index.js
+++ scripts/index.js
@@ -2,41 +2,54 @@ console.log('Popup Page Running');
 
 
 let popDetail =  document.querySelector("#popInfo"),
+    taskActive =  document.querySelector("#taskActive"),
+    taskInactive =  document.querySelector("#taskInactive"),
     taskUrl = document.querySelector('#clisha-task'),
     deactivateButton =  document.querySelector('#clisha-deactivate');
 
     
 // Action on Popup 
 chrome.storage.sync.get('task', (item) => {
-    console.log(item, 'Available')
     if (Object.keys(item).length) {
         let task = item.task;
-       popDetail.innerHTML=`Task #${task.task_code} is currently active. <a href="#" id="clisha-deactivate">Click here</a>  to  deactivate the task `;
+        activateTask(task);
+        //  popDetail.innerHTML=`Task #${task.task_code} is currently active. <a href="#" id="clisha-deactivate">Click here</a>  to  deactivate the task `;
     }
 });
 
+function activateTask(task) {
+    let taskId = document.querySelector('#task-code-ied7yghfubj'),
+        taskPoint = document.querySelector('#task-point-askjdcn'),
+        taskSeach = document.querySelector('#task-search-idhfonksdx');
 
-if(taskUrl){
-    taskUrl.addEventListener('click', function () {
-        let baseUrl =  "https://clisha-stagging.netlify.app/dashboard/task";
-        chrome.tabs.create({ url: baseUrl });
-    })
+    taskActive.style.display = "block";
+    taskInactive.style.display = "none";
+    taskId.innerHTML = `${task.task_code}`;
+    taskPoint.innerHTML = `${task.points}`;
+    taskSeach.innerHTML = `${JSON.parse(task.google_search).title}`;
 }
 
-if(deactivateButton){
-    deactivateButton.addEventListener('click', function () {
-        chrome.storage.sync.clear(function() {
-           popDetail.innerHTML=`No active task <a href="#" id="clisha-task">click here</a>  to get and activate a task`;
-            var error = chrome.runtime.lastError;
-            if (error) console.log(error); throw error;
-        });
-    
-    })
-}
+taskUrl.addEventListener('click', function () {
+    let baseUrl =  "https://clisha-stagging.netlify.app/dashboard/task";
+    chrome.tabs.create({ url: baseUrl });
+})
+
+deactivateButton.addEventListener('click', function () {
+    chrome.storage.sync.clear(function() {
+        
+        taskActive.style.display = "none";
+        taskInactive.style.display = "block";
+        chrome.runtime.reload()
+        //popDetail.innerHTML=`No active task <a href="#" id="clisha-task">click here</a>  to get and activate a task`;
+        var error = chrome.runtime.lastError;
+        if (error) console.log(error); throw error;
+    });
+
+});
 
 chrome.runtime.onMessage.addListener( (message, sender, sendResponse) => { 
     let task = message.task;
-    if(task) popDetail.innerHTML=`Task #${task.task_code} is currently active. <a href="#" id="clisha-deactivate">Click here</a>  to  deactivate the task `;
+    activateTask(task)
     return true;
 });
 
