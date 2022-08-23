2677b255ebf61d305dd995f3609c05548c8a5f67
vearse <versatilekaywize94@gmail.com>
2022-08-23 08:07:14 +0100

    Adding configuration for Dev and Client

---
+++ scripts/config.js
@@ -0,0 +1,10 @@
+
+const ClientConfig = {
+    dashboardUrl: 'https://clisha-client-user.netlify.app/dashboard/'
+}
+
+const config = ClientConfig;
+
+
+module.exports =  config;
+// export default config;
