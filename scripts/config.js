90f1e90ee37896f79008d0c56cc4fc752158dde9
vearse <versatilekaywize94@gmail.com>
2022-09-14 09:58:56 +0100

    Preparing Webstore packages

--- scripts/config.js
+++
@@ -1,9 +0,0 @@
-
-const ClientConfig = {
-    dashboardUrl: 'https://clisha-client-user.netlify.app/dashboard/',
-    baseServer: 'https://clisha-client-user.netlify.app/dashboard/'
-}
-
-const config = ClientConfig;
-
-// export default config;
